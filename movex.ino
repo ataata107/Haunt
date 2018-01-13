4f7927037d1de41a4a8d12c1e27842a15e4b67cc
Shazeb <ataata107@gmail.com>
2018-01-13 15:51:44 +0530

    moves in x

---
+++ movex.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+Servo haunting_eye;
+
+void setup() {
+  Serial.begin(9600);
+  haunting_eye.attach(9);
+  haunting_eye.write(90);
+}
+ 
+unsigned int integerValue=0;  // Max value is 65535
+char incomingByte;
+ 
+void loop() {
+  if (Serial.available() > 0) {   // something came across serial
+    integerValue = 0;         // throw away previous integerValue
+    while(1) {            // force into a loop until 'n' is received
+      incomingByte = Serial.read();
+      if (incomingByte == '\n') break;   // exit the while(1), we're done receiving
+      if (incomingByte == -1) continue;  // if no characters are in the buffer read() returns -1
+      integerValue *= 10;  // shift left 1 decimal place
+      // convert ASCII to integer, add, and shift left 1 decimal place
+      integerValue = ((incomingByte - 48) + integerValue);
+    }
+    Serial.println(integerValue);   // Do something with the value
+    haunting_eye.write(integerValue);
+  }
+}
