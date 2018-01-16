5398220fcb4520ba282f8069808adfaa370c4ad8
Shazeb <ataata107@gmail.com>
2018-01-16 22:05:13 +0530

    takes decimal serial value

--- movex/movex.ino
+++ movex/movex.ino
@@ -1,45 +1,30 @@
-#include <Servo.h>
-Servo haunting_eye;
+#include<Servo.h>
+Servo eye;
+int servoPin = 9;
+char received;
+String inString;
+float value;
 
 void setup() {
-  Serial.begin(9600);
-  haunting_eye.attach(9);
-  haunting_eye.write(90);
+eye.attach(servoPin);
+Serial.begin(9600);
 }
- 
-float integerValue=0;  // Max value is 65535
-float decimalValue=0;
-int n;
-char incomingByte;
- 
+
 void loop() {
-  if (Serial.available() > 0) {   // something came across serial
-    integerValue = 0;     // throw away previous integerValue
-    decimalValue = 0;
-    n=1;
-    while(1) {            // force into a loop until 'n' is received
-      incomingByte = Serial.read();
-      if (incomingByte == '\n') break;   // exit the while(1), we're done receiving
-      if (incomingByte == -1) continue;  // if no characters are in the buffer read() returns -1
-      if (incomingByte == '.'){
-        while(1){
-          incomingByte = Serial.read();
-          if (incomingByte == '\n') break;
-          
-          decimalValue *= 10;
-          
-          decimalValue = ((incomingByte - 48) + decimalValue);
-          
-          n=n+1;
-        }
-        (integerValue) = (integerValue) + (decimalValue)/pow(10,n);
-        break;
-      }
-      integerValue *= 10;  // shift left 1 decimal place
-      // convert ASCII to integer, add, and shift left 1 decimal place
-      integerValue = ((incomingByte - 48) + integerValue);
+  
+while (Serial.available() > 0) {
+    int inChar = Serial.read();
+      // convert the incoming byte to a char and add it to the string:
+    inString += (char)inChar;
+    
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      value = inString.toFloat();
+      Serial.println(value);
+      Serial.println(inString);
+      eye.write(value);
+      // clear the string for new input:
+      inString = "";
     }
-    Serial.println(integerValue);   // Do something with the value
-    haunting_eye.write(integerValue);
   }
 }
