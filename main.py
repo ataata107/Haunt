e70033731c9053d49de46a993b38f517197f8008
Shazeb <ataata107@gmail.com>
2018-02-09 00:10:44 +0530

    no change

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import numpy as np
 import cv2
 import serial
 from PID import PIDController
-ser = serial.Serial('COM10',9600)
+ser = serial.Serial('/dev/ttyACM0',9600)
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
 #https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-face_cascade = cv2.CascadeClassifier('C:\\Users\\ataata107\\Downloads\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
 val=90
+val_y=90
 Center_x=0
+Center_y=0
 #-----------------------------------PID------------------------------------------
 pid = PIDController(proportional = 0.015, derivative_time = 0, integral_time=0)
 pid.vmin, pid.vmax = -10, 10
@@ -18,6 +20,15 @@ TDifference = pid.setpoint
 baseAngle = 90
 
 pidout = 0
+
+#-----------------------------------PID---y------------------------------------------
+pid_y = PIDController(proportional = 0.015, derivative_time = 0, integral_time=0)
+pid_y.vmin, pid_y.vmax = -10, 10
+pid_y.setpoint = 0.0   #aTargetDifference(m)
+TDifference_y = pid_y.setpoint
+baseAngle_y = 90
+
+pidout_y = 0
 while 1:
     center_x=[]
     center_y=[]
@@ -25,6 +36,7 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     CENTER=img.shape[0]/2
+    CENTER_y=img.shape[1]/2
     
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -32,6 +44,8 @@ while 1:
         center_x.append(x)
         center_y.append(y)
     print(len(center_x))
+    print(len(center_y))
+#-----------------X process-----------------------------------------
     if(len(center_x)>0):
         a=center_x[0]
         for i in range((len(center_x))-1):
@@ -52,9 +66,34 @@ while 1:
             
     if(val>180 or val<0):
         val=90
-    ser.write("%s\n"%val)     
-    print("val",val)    
+#---------------------------Y process----------------------------------
+    if(len(center_y)>0):
+        b=center_y[0]
+        for i in range((len(center_y))-1):
+            if(abs(CENTER_y-center_y[i+1])<abs(CENTER_y-b)):
+                b=center_x[i+1]
+        print(b, "Following center_y",CENTER_y)
+        current_difference_y = (b-CENTER_y)
+        print ("Difference left y%s" % (TDifference_y - current_difference_y))
+    
+        pidout_y = pid.compute_output(current_difference_y)
+        pidout_y += baseAngle_y
+        val_y=pidout_y
+        if(b > CENTER_y ):
+            print("<<<<<<<<<")
+            
+        elif(b< CENTER_y):
+            print(">>>>>>>>>")
+            
+    if(val_y>180 or val_y<0):
+        val_y=90
+#-------------------------------------------------
+
+
+    ser.write("%s %s\n"%val)     
+    print("val",val,"val_y",val_y)    
     baseAngle=val
+    baseAngle_y=val_y
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
---
+++ movex/main_move/main_move.ino
@@ -0,0 +1,38 @@
+#include<Servo.h>
+Servo eye;
+Servo eye_y;
+int servoPin = 9;
+int servoPin_y = 10;
+char received;
+String inString;
+String inString_y;
+float value;
+float value_y;
+boolean flag= false;
+
+void setup() {
+eye.attach(servoPin);
+eye_y.attach(servoPin_y);
+Serial.begin(9600);
+}
+
+void loop() {
+  
+while (Serial.available() > 0) {
+    int inChar = Serial.read();
+      // convert the incoming byte to a char and add it to the string:
+    if (flag){
+       
+    inString += (char)inChar;
+      
+    // if you get a newline, print the string, then the string's value:
+    if (inChar == '\n') {
+      value = inString.toFloat();
+      Serial.println(value);
+      Serial.println(inString);
+      eye.write(value);
+      // clear the string for new input:
+      inString = "";
+    }
+  }
+}
